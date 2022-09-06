b49e134a0af14aa57ad34d7e84186695757da74b
Dario Turin <dturin1997@gmail.com>
2022-09-06 15:28:20 -0500

    Agregar archivos de proyecto.

---
+++ AMM_Lab_S3/AMM_Lab_S3/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AMM_Lab_S3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GridCode();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
